00f341d807c488b20dc9c2bfc2b5f85fb86325ec
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2020-03-28 07:02:46 +0100

    Major Updates

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
 )
 
+app.title = "Maroc Covid19"
 app.config.suppress_callback_exceptions = True
+# app.scripts.config.serve_locally = False
+# app.scripts.append_script({"external_url": "https://cdn.plot.ly/plotly-locale-ar-latest.js"})
