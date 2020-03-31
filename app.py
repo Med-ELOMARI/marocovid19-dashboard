fe05f1988dadcf3cd01becd66d28eea1b24a05e3
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2020-03-31 07:20:00 +0200

    :chart_with_upwards_trend: Revert ":chart_with_upwards_trend: Changed GA ID"
    This reverts commit 786fd94e

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ app.index_string = """
         {%favicon%}
         
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-226566305-1"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152784336-1"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', 'UA-226566305-1');
+          gtag('config', 'UA-152784336-1');
         </script>
 
 
